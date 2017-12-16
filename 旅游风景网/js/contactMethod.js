81927221ec5f7ae4527c47a90c2f2aefef54191c
PatrickNg <PatrickNgZh@outlook.com>
2017-12-16 19:27:21 +0800

    update JavaScript images pop

--- 旅游风景网/js/contactMethod.js
+++ 旅游风景网/js/contactMethod.js
@@ -1,10 +1,17 @@
-window.onload=function () {
-    document.getElementById("weibo").onmouseover=function () {
-        document.getElementById("weibo").style.backgroundImage = "url(../images/weibo_hl.svg)";
-        console.log("over");
-    }
-    document.getElementById("weibo").onmouseout=function () {
-        document.getElementById("weibo").style.backgroundImage = "url(../images/weibo.svg)";
-        console.log("out");
-    }
-}
+window.onload = function () {
+    document.getElementById("weibo").onmouseover = function () {
+        var temp = document.getElementById("weibo");
+        var img = document.getElementById("weibo-pop");
+        temp.style.backgroundImage = "url(images/weibo_hl.svg)";
+        // console.log("over");
+        img.style.display = "block";
+    };
+    document.getElementById("weibo").onmouseout = function () {
+        var temp = document.getElementById("weibo");
+        var img = document.getElementById("weibo-pop");
+        temp.style.backgroundImage = "url(images/weibo.svg)";
+        // console.log("out");
+        img.style.display = "none";
+    };
+};
+
