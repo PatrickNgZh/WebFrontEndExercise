1143c4644388f446c0de10a2cc24341691def358
PatrickNg <PatrickNgZh@outlook.com>
2017-12-17 12:05:24 +0800

    add the scrollTop() of jquery

--- 旅游风景网/js/hideNavigation.js
+++ 旅游风景网/js/hideNavigation.js
@@ -1,13 +1,15 @@
 var scrollFunc = function (e) {
     // var t = document.documentElement.scrollTop || document.body.scrollTop;  //获取距离页面顶部的距离
     var header = document.getElementById("header");
-    var status =getComputedStyle(header).getPropertyValue("opacity");
+    var status = getComputedStyle(header).getPropertyValue("opacity");
     if (e.wheelDelta > 0) { //当滑轮向上滚动时
         status = 1;
     }
     if (e.wheelDelta < 0) { //当滑轮向下滚动时
-        status-=0.08;
+        status -= 0.08;
+        // console.log($(document).scrollTop()+ " px");
+        // document不用加双引号
     }
     header.style.opacity = status;
-}
+};
 window.onmousewheel = document.onmousewheel = scrollFunc;
