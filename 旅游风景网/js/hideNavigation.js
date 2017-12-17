3c543847a689b28dd4983e84fbdc869549bc11cb
PatrickNg <PatrickNgZh@outlook.com>
2017-12-17 16:41:17 +0800

    finish the navigation

--- 旅游风景网/js/hideNavigation.js
+++ 旅游风景网/js/hideNavigation.js
@@ -4,8 +4,14 @@ var scrollFunc = function (e) {
     var status = getComputedStyle(header).getPropertyValue("opacity");
     if (e.wheelDelta > 0) { //当滑轮向上滚动时
         status = 1;
+        if($(document).scrollTop()<421){
+            header.style.backgroundColor="";
+        }
+        else{
+            header.style.backgroundColor="#005c30";
+        }
     }
-    if (e.wheelDelta < 0) { //当滑轮向下滚动时
+    else { //当滑轮向下滚动时
         status -= 0.08;
         // console.log($(document).scrollTop()+ " px");
         // document不用加双引号
